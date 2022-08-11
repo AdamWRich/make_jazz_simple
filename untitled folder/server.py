5cee34206e0c5306daaecc167361919d7374d610
Adam Rich <103798175+AdamWRich@users.noreply.github.com>
2022-08-10 20:18:05 -0400

    initial commit

---
+++ untitled folder/server.py
@@ -0,0 +1,14 @@
+from flask_app import app
+from flask import session
+from flask_app.controllers import users
+from flask_app.controllers import paintings
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
